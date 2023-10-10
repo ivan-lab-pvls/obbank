558e45ed62c08c81722a2129f90a936b728199cb
ilobosok <121606196+ivan-lab-pvls@users.noreply.github.com>
2023-10-10 12:48:45 +0200

    x

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,12 @@
 import 'dart:async';
-
 import 'package:bank/app_theme.dart';
-import 'package:bank/firebase_options.dart';
-import 'package:bank/notification_service.dart';
 import 'package:bank/router.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  await NotificationServiceFb().activate();
-
   final sharedPrefs = await SharedPreferences.getInstance();
   final StreamController<bool> updateBalanceStream =
       StreamController<bool>.broadcast();
--- lib/pages/settings_page/net_screen.dart
+++ lib/pages/settings_page/net_screen.dart
@@ -2,7 +2,7 @@
 
 import 'package:bank/build_context_ext.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 class NetScreen extends StatefulWidget {
   const NetScreen({super.key, required this.u});
@@ -19,39 +19,39 @@ class _NetScreenState extends State<NetScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      // body: SafeArea(
-      //   bottom: false,
-      //   child: Stack(
-      //     children: [
-      //       InAppWebView(
-      //         onLoadStop: (controller, url) {
-      //           controller.evaluateJavascript(
-      //               source: "javascript:(function() { " +
-      //                   "var ele=document.getElementsByClassName('docs-ml-header-item docs-ml-header-drive-link');" +
-      //                   "ele[0].parentNode.removeChild(ele[0]);" +
-      //                   "var footer = document.getelementsbytagname('footer')[0];" +
-      //                   "footer.parentnode.removechild(footer);" +
-      //                   "})()");
-      //         },
-      //         onProgressChanged: (controller, progress) => setState(() {
-      //           _progress = progress;
-      //         }),
-      //         initialUrlRequest: URLRequest(
-      //           url: Uri.parse(widget.u),
-      //         ),
-      //       ),
-      //       if (_progress != 100)
-      //         Container(
-      //           color: Colors.white,
-      //           child: Center(
-      //             child: CircularProgressIndicator(
-      //               color: context.theme.primaryColor,
-      //             ),
-      //           ),
-      //         ),
-      //     ],
-      //   ),
-      // ),
+      body: SafeArea(
+        bottom: false,
+        child: Stack(
+          children: [
+            InAppWebView(
+              onLoadStop: (controller, url) {
+                controller.evaluateJavascript(
+                    source: "javascript:(function() { " +
+                        "var ele=document.getElementsByClassName('docs-ml-header-item docs-ml-header-drive-link');" +
+                        "ele[0].parentNode.removeChild(ele[0]);" +
+                        "var footer = document.getelementsbytagname('footer')[0];" +
+                        "footer.parentnode.removechild(footer);" +
+                        "})()");
+              },
+              onProgressChanged: (controller, progress) => setState(() {
+                _progress = progress;
+              }),
+              initialUrlRequest: URLRequest(
+                url: Uri.parse(widget.u),
+              ),
+            ),
+            if (_progress != 100)
+              Container(
+                color: Colors.white,
+                child: Center(
+                  child: CircularProgressIndicator(
+                    color: context.theme.primaryColor,
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
